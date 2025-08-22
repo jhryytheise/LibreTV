0f244ddce4eabf6cfadd921a4c00f179695b0561
jhryytheise <50068838+jhryytheise@users.noreply.github.com>
2025-08-23 03:34:24 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -114,10 +114,6 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-    }
     ckzy: {
          api: 'https://www.ckzy1.com',
          name: 'CK资源',
